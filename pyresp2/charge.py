852a0164fbbf5a7829868e58f9977c9b5c7207c9
Zhiyi Wu <zhiyi.wu@bioch.ox.ac.uk>
2021-11-01 14:32:13 +0000

    update

--- pyresp2/charge.py
+++ pyresp2/charge.py
@@ -1,8 +1,9 @@
 import os
 from glob import glob
 from os.path import join
+from pkg_resources import resource_filename
+import MDAnalysis as mda
 import numpy as np
-import shutil
 
 def gen_charge(config):
     file_list = glob(join('charge', '*.xyz'))
@@ -44,6 +45,52 @@ RESP
            resp2_sol=config['charge']['resp2_sol'],
            i=os.path.basename(base).split('_')[-1],
            crd='\n'.join(crd)))
+
+    with open(resource_filename(__name__, 'data/RESP.in'), 'r') as f:
+        Multiwfn_in = f.read()
+
+    with open('charge/Multiwfn_resp_HF.in', 'w') as f:
+        f.write(Multiwfn_in.replace('conflist.txt', 'conflist_HF.txt'))
+
+    with open('charge/Multiwfn_resp_Vac.in', 'w') as f:
+        f.write(Multiwfn_in.replace('conflist.txt', 'conflist_Vac.txt'))
+
+    with open('charge/Multiwfn_resp_Sol.in', 'w') as f:
+        f.write(Multiwfn_in.replace('conflist.txt', 'conflist_Sol.txt'))
+
+def _find_RESP(text):
+    with open(text, 'r') as f:
+        lines = f.read().split('\n')
+    charge_list = []
+    start = False
+    for line in lines:
+        if 'Center       Charge' in line:
+            start = True
+            continue
+        if 'Sum of charges' in line:
+            return np.array(charge_list)
+        if start:
+            charge_list.append(float(line.split()[-1]))
+
+def read_charge(mol, multiwfn_log):
+    u = mda.Universe(mol)
+    charges = _find_RESP(multiwfn_log)
+    assert len(charges) == len(u.atoms)
+    u.atoms.charges = charges
+    return u
+
+def read_all_charges(mol, HF='charge/Multiwfn_resp_HF.log',
+                     Vac='charge/Multiwfn_resp_Vac.log',
+                     Sol='charge/Multiwfn_resp_Sol.log'):
+    HF_mol = read_charge(mol, HF)
+    HF_mol.atoms.write('charge/HF.mol2')
+    Vac_mol = read_charge(mol, Vac)
+    Sol_mol = read_charge(mol, Sol)
+    u = mda.Universe(mol)
+    u.atoms.charges = (Vac_mol.atoms.charges + Sol_mol.atoms.charges) / 2
+    u.atoms.write('charge/RESP2.mol2')
+
+
 
 
 
--- pyresp2/tests/test_charge.py
+++ pyresp2/tests/test_charge.py
@@ -1,11 +1,12 @@
 import os.path
 import shutil
 
+import numpy as np
 import pytest
 import configparser
 from pkg_resources import resource_filename
 
-from pyresp2.charge import gen_charge
+from pyresp2.charge import gen_charge, read_charge, read_all_charges
 
 @pytest.fixture(scope='class')
 def config():
@@ -22,3 +23,17 @@ def test_gen_charge(config):
     for i in range(5):
         assert os.path.isfile('charge/charge_{}.gjf'.format(i))
     shutil.rmtree('charge')
+
+def test_read_charge():
+    mol = read_charge(resource_filename(__name__, 'test_data/opt.mol2'),
+                      resource_filename(__name__, 'test_data/RESP.log'))
+    assert np.isclose(mol.atoms.charges[0], -0.2506055214, 0.01)
+
+def test_read_charge():
+    os.makedirs('charge', exist_ok=True)
+    read_all_charges(resource_filename(__name__, 'test_data/opt.mol2'),
+                     HF=resource_filename(__name__, 'test_data/RESP.log'),
+                     Vac=resource_filename(__name__, 'test_data/RESP.log'),
+                     Sol=resource_filename(__name__, 'test_data/RESP.log'))
+    assert os.path.isfile('charge/RESP2.mol2')
+    assert os.path.isfile('charge/HF.mol2')
